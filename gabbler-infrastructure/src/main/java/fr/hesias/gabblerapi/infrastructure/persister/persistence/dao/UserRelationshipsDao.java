e52e7e8408b1885af66f0f1067f84392ee5e5cf6
Sinsay63 <yanis.houdier@gmail.com>
2023-03-20 03:26:23 +0100

    oubli

--- gabbler-infrastructure/src/main/java/fr/hesias/gabblerapi/infrastructure/persister/persistence/dao/UserRelationshipsDao.java
+++ gabbler-infrastructure/src/main/java/fr/hesias/gabblerapi/infrastructure/persister/persistence/dao/UserRelationshipsDao.java
@@ -2,11 +2,11 @@ package fr.hesias.gabblerapi.infrastructure.persister.persistence.dao;
 
 import fr.hesias.gabblerapi.infrastructure.persister.persistence.repository.UserRelationshipsRepository;
 
-public class UserRelationshipsDao {
+public class UserRelationShipsDao {
 
     private final UserRelationshipsRepository userRelationshipsRepository;
 
-    public UserRelationshipsDao(final UserRelationshipsRepository userRelationshipsRepository) {
+    public UserRelationShipsDao(UserRelationshipsRepository userRelationshipsRepository) {
         this.userRelationshipsRepository = userRelationshipsRepository;
     }
 }
