c73f76e623a94a271bec524febefe12402c0c636
Sinsay63 <yanis.houdier@gmail.com>
2023-04-26 15:14:04 +0200

    rajout uuid user au token

--- gabbler-application/src/main/java/fr/hesias/gabblerapi/application/security/service/JwtService.java
+++ gabbler-application/src/main/java/fr/hesias/gabblerapi/application/security/service/JwtService.java
@@ -85,6 +85,7 @@ public class JwtService
                    .setClaims(claims)
                    .setSubject(userName)
                    .claim("roles", userInfosAccessorAdapter.getUserByEmail(userName).getDomainUser().getRoles())
+                   .claim("uuid", userInfosAccessorAdapter.getUserByEmail(userName).getDomainUser().getUuid())
                    .setIssuedAt(new Date(System.currentTimeMillis()))
                    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
                    .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
