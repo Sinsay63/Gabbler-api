f7cd6008d98e6a546cf444fe12be96f38b44cba1
Sinsay63 <yanis.houdier@gmail.com>
2023-05-23 22:44:04 +0200

    edit relations get

--- gabbler-application/src/main/java/fr/hesias/gabblerapi/application/api/service/RelationshipsApiDelegateImpl.java
+++ gabbler-application/src/main/java/fr/hesias/gabblerapi/application/api/service/RelationshipsApiDelegateImpl.java
@@ -2,46 +2,40 @@ package fr.hesias.gabblerapi.application.api.service;
 
 import fr.hesias.gabblerapi.application.adapter.RelationshipsAccessorAdapter;
 import fr.hesias.gabblerapi.desc.api.server.RelationshipApiDelegate;
+import fr.hesias.gabblerapi.desc.api.server.model.RelationUser;
 import fr.hesias.gabblerapi.domain.model.DomainUserRelationshipsCreation;
 import fr.hesias.gabblerapi.domain.result.DomainUserRelationshipsCreationResult;
 import org.springframework.http.ResponseEntity;
 
-import java.util.HashMap;
-
 import static fr.hesias.gabblerapi.domain.model.DomainAccessStatus.OK;
 
-public class RelationshipsApiDelegateImpl implements RelationshipApiDelegate
-{
+public class RelationshipsApiDelegateImpl implements RelationshipApiDelegate {
 
     private final RelationshipsAccessorAdapter relationshipsAccessorAdapter;
 
-    public RelationshipsApiDelegateImpl(RelationshipsAccessorAdapter relationshipsAccessorAdapter)
-    {
+    public RelationshipsApiDelegateImpl(RelationshipsAccessorAdapter relationshipsAccessorAdapter) {
 
         this.relationshipsAccessorAdapter = relationshipsAccessorAdapter;
     }
 
     @Override
-    public ResponseEntity<String> relationshipsCUD(String userUuid, String userRelatedUuid, String relation)
-    {
+    public ResponseEntity<String> relationshipsCUD(String userUuid, String userRelatedUuid, String relation) {
 
         DomainUserRelationshipsCreation domainUserRelationshipsCreation = new DomainUserRelationshipsCreation(
                 userUuid,
                 userRelatedUuid,
                 relation);
         relationshipsAccessorAdapter.relationshipsCUD(new DomainUserRelationshipsCreationResult(OK,
-                                                                                                domainUserRelationshipsCreation));
+                domainUserRelationshipsCreation));
         return ResponseEntity.ok("OK");
     }
 
     @Override
-    public ResponseEntity<Object> getRelationByUserAndUserRelated(String userUuid, String userRelatedUuid)
-    {
+    public ResponseEntity<RelationUser> getRelationByUserAndUserRelated(String userUuid, String userRelatedUuid) {
+        RelationUser response = new RelationUser();
+        response.setType(
+                relationshipsAccessorAdapter.getRelationByUserAndUserRelated(userUuid, userRelatedUuid));
 
-        HashMap<String, String> response = new HashMap<>();
-        response.put("relation",
-                     relationshipsAccessorAdapter.getRelationByUserAndUserRelated(userUuid,
-                                                                                  userRelatedUuid));
         return ResponseEntity.ok(response);
     }
 
